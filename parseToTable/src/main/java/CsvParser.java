d279ac5d3070767bb1bcff95ff1524ff49ce03f7
Witold Zolnowski <blue3656@gmail.com>
2021-07-27 21:53:57 +0100

    read file & return array of elements

--- parseToTable/src/main/java/CsvParser.java
+++ parseToTable/src/main/java/CsvParser.java
@@ -1,3 +1,20 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
 public class CsvParser {
-  
+ public String[] parse(Path pathToFile) {
+   try(BufferedReader br = Files.newBufferedReader(pathToFile)){
+     return br.readLine().split(",");
+//     String line = "";
+//     while((line =  br.readLine()) != null){
+//       str += line;
+//     };
+//     return str;
+   } catch (IOException e) {
+     e.printStackTrace();
+   }
+   return null;
+ }
 }
--- parseToTable/src/test/java/CsvParserTest.java
+++ parseToTable/src/test/java/CsvParserTest.java
@@ -1,12 +1,31 @@
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import java.nio.file.Path;
+import java.util.Arrays;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class CsvParserTest {
+  CsvParser csvParser;
+
+  @BeforeEach
+  void setup() {
+    csvParser = new CsvParser();
+  }
 
   @Test
-  public void returnsTrue(){
+  public void returnsTrue() {
     assertTrue(true);
+  }
+
+  @Test
+  public void returnsString() {
+    String[] result = csvParser.parse(Path.of("src/test/resources/data.csv"));
+    String[][] expected = {{"Kushi Tsuru, Mon-Sun 11:30 am - 9 pm"}, {"Osakaya Restaurant", "Mon-Thu, Sun 11:30 am - 9 pm  / Fri-Sat 11:30 am - 9:30 pm"}};
+
+    assertEquals(Arrays.deepToString(expected), Arrays.deepToString(result));
   }
 
 }
