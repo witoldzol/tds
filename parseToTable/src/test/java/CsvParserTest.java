9d30a7fe895eccfe055066d3a0a0b9e3c530709d
Witold Zolnowski <blue3656@gmail.com>
2021-07-27 21:28:26 +0100

    java project setup

---
+++ parseToTable/src/test/java/CsvParserTest.java
@@ -0,0 +1,12 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CsvParserTest {
+
+  @Test
+  public void returnsTrue(){
+    assertTrue(true);
+  }
+
+}
