edd7072d92f88c6f1b55340cb921d6685237ce78
Witold Zolnowski <blue3656@gmail.com>
2021-07-31 12:20:26 +0100

    remove redundant function

--- parseToTable/src/main/java/CsvParser.java
+++ parseToTable/src/main/java/CsvParser.java
@@ -8,7 +8,9 @@ import java.util.stream.Stream;
 public class CsvParser {
   public List<String[]> parse(Path pathToFile) {
     try (Stream<String> lines = Files.lines(pathToFile)) {
-      return lines.map(str -> removeChar("\"", str))
+      return lines.map(str -> str.replaceAll("\"", ""))
+              // limit is the MAX number of array items returned after split operation
+              // pattern is applied max limit -1 times
               .map(str -> str.split(",", 2))
               .collect(Collectors.toList());
     } catch (IOException e) {
@@ -17,8 +19,5 @@ public class CsvParser {
     return null;
   }
 
-  private String removeChar(String pattern, String str) {
-    return str.replaceAll(pattern, "");
-  }
 }
 
