3f768f58d35750d2b77cb5c85c8ab591ecd78417
Witold Zolnowski <blue3656@gmail.com>
2021-07-31 12:16:12 +0100

    refactor to streams

--- parseToTable/src/main/java/CsvParser.java
+++ parseToTable/src/main/java/CsvParser.java
@@ -1,19 +1,16 @@
-import java.io.BufferedReader;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class CsvParser {
-  public ArrayList<String[]> parse(Path pathToFile) {
-    try (BufferedReader br = Files.newBufferedReader(pathToFile)) {
-      ArrayList<String[]> result = new ArrayList<>();
-      String line = "";
-      while ((line = br.readLine()) != null) {
-        String[] temp = removeChar("\"",line).split(",",2);
-                result.add(temp);
-      } ;
-      return result;
+  public List<String[]> parse(Path pathToFile) {
+    try (Stream<String> lines = Files.lines(pathToFile)) {
+      return lines.map(str -> removeChar("\"", str))
+              .map(str -> str.split(",", 2))
+              .collect(Collectors.toList());
     } catch (IOException e) {
       e.printStackTrace();
     }
--- parseToTable/src/test/java/CsvParserTest.java
+++ parseToTable/src/test/java/CsvParserTest.java
@@ -17,18 +17,13 @@ class CsvParserTest {
   }
 
   @Test
-  public void returnsTrue() {
-    assertTrue(true);
-  }
-
-  @Test
   public void returnsString() {
     String[] s1 = {"Kushi Tsuru", "Mon-Sun 11:30 am - 9 pm"};
     String[] s2 = {"Osakaya Restaurant", "Mon-Thu, Sun 11:30 am - 9 pm  / Fri-Sat 11:30 am - 9:30 pm"};
     String[] s3 = {"Kyoto Sushi", "Mon-Thu 11 am - 10:30 pm  / Fri 11 am - 11 pm  / Sat 11:30 am - 11 pm  / Sun 4:30 pm - 10:30 pm"};
     List<String[]> expected = List.of(s1, s2, s3);
 
-    ArrayList<String[]> result = csvParser.parse(Path.of("src/test/resources/data.csv"));
+    List<String[]> result = csvParser.parse(Path.of("src/test/resources/data.csv"));
 
     assertArrayEquals(expected.toArray(), result.toArray());
   }
