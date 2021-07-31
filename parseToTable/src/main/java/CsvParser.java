84d9a28e17e33852f44908de325c818b5f4b1cfd
Witold Zolnowski <blue3656@gmail.com>
2021-07-31 12:04:47 +0100

    split only on first comma, remove random double quotes

--- parseToTable/src/main/java/CsvParser.java
+++ parseToTable/src/main/java/CsvParser.java
@@ -3,22 +3,25 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.ArrayList;
-import java.util.Arrays;
 
 public class CsvParser {
- public ArrayList<String[]> parse(Path pathToFile) {
-   try(BufferedReader br = Files.newBufferedReader(pathToFile)){
-//     return br.readLine().split(",");
-     ArrayList<String[]> result = new ArrayList<>();
-     String line = "";
-     while((line =  br.readLine()) != null){
-       String[] temp = line.split(",");
-       result.add(temp);
-     };
-     return result;
-   } catch (IOException e) {
-     e.printStackTrace();
-   }
-   return null;
- }
+  public ArrayList<String[]> parse(Path pathToFile) {
+    try (BufferedReader br = Files.newBufferedReader(pathToFile)) {
+      ArrayList<String[]> result = new ArrayList<>();
+      String line = "";
+      while ((line = br.readLine()) != null) {
+        String[] temp = removeChar("\"",line).split(",",2);
+                result.add(temp);
+      } ;
+      return result;
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+    return null;
+  }
+
+  private String removeChar(String pattern, String str) {
+    return str.replaceAll(pattern, "");
+  }
 }
+
--- parseToTable/src/test/java/CsvParserTest.java
+++ parseToTable/src/test/java/CsvParserTest.java
@@ -3,10 +3,10 @@ import org.junit.jupiter.api.Test;
 
 import java.nio.file.Path;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class CsvParserTest {
   CsvParser csvParser;
@@ -25,7 +25,7 @@ class CsvParserTest {
   public void returnsString() {
     String[] s1 = {"Kushi Tsuru", "Mon-Sun 11:30 am - 9 pm"};
     String[] s2 = {"Osakaya Restaurant", "Mon-Thu, Sun 11:30 am - 9 pm  / Fri-Sat 11:30 am - 9:30 pm"};
-    String[] s3 = {"Kyoto Sushi", "Mon - Thu 11am - 10: 30pm / Fri 11am - 11pm / Sat 11:30 am - 11 pm / Sun 4:30 pm - 10:30 pm"};
+    String[] s3 = {"Kyoto Sushi", "Mon-Thu 11 am - 10:30 pm  / Fri 11 am - 11 pm  / Sat 11:30 am - 11 pm  / Sun 4:30 pm - 10:30 pm"};
     List<String[]> expected = List.of(s1, s2, s3);
 
     ArrayList<String[]> result = csvParser.parse(Path.of("src/test/resources/data.csv"));
