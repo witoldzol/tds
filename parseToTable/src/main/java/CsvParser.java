cb5cb0bd56b89d723deb99b160d81bb241da026b
Witold Zolnowski <blue3656@gmail.com>
2021-07-31 12:27:04 +0100

    refactor into single responsibility methods

--- parseToTable/src/main/java/CsvParser.java
+++ parseToTable/src/main/java/CsvParser.java
@@ -8,15 +8,22 @@ import java.util.stream.Stream;
 public class CsvParser {
   public List<String[]> parse(Path pathToFile) {
     try (Stream<String> lines = Files.lines(pathToFile)) {
-      return lines.map(str -> str.replaceAll("\"", ""))
-              // limit is the MAX number of array items returned after split operation
-              // pattern is applied max limit -1 times
-              .map(str -> str.split(",", 2))
-              .collect(Collectors.toList());
+      return splitIntoTwoElements(removeExtraQuotes(lines));
     } catch (IOException e) {
       e.printStackTrace();
     }
     return null;
+  }
+
+  private List<String[]> splitIntoTwoElements(Stream<String> lines) {
+    // limit is the MAX number of array items returned after split operation
+    // pattern is applied max limit -1 times
+    return lines.map(str -> str.split(",", 2))
+            .collect(Collectors.toList());
+  }
+
+  private Stream<String> removeExtraQuotes(Stream<String> lines) {
+    return lines.map(str -> str.replaceAll("\"", ""));
   }
 
 }
